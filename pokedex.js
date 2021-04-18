0fcef37e2c4669f47b5dccfad157679bfb25b220
unknown <yvonnegamboeck@web.de>
2021-04-18 19:14:49 +0200

    add some properties

--- pokedex.js
+++ pokedex.js
@@ -14,4 +14,10 @@ async function loadPokemon() {
 function renderPokemonInfo() {
 document.getElementById('pokemonname').innerHTML = currentPokemon['name']; //name is defined at api
 document.getElementById('pokemonImg').src = currentPokemon['sprites'] ['front_default']; //load img from api
+document.getElementById('pokemonWeight').innerHTML = currentPokemon['weight'];
+document.getElementById('pokemonHeight').innerHTML = currentPokemon['height'];
+document.getElementById('pokemonOrder').innerHTML = currentPokemon['order'];
+document.getElementById('pokemonAbility1').innerHTML = currentPokemon['abilities'] [0]['ability'] ['name'];
+document.getElementById('pokemonAbility2').innerHTML = currentPokemon['abilities'] [1]['ability'] ['name'];
+document.getElementById('pokemonAbility3').innerHTML = currentPokemon['abilities'] [2]['ability'] ['name'];
 }
